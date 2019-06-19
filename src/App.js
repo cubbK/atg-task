79c6205d3c21517173ec8d52bf2338e51cc4d4d2
cubbk <danpopa1998@gmail.com>
2019-06-19 21:10:17 +0300

    fix test not passing because of redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import store from './redux/store'
+import { Provider } from 'react-redux'
 import { GameTypeSearch } from './containers/GameTypeSearch'
 import { GamesList } from './containers/GamesList'
 import Typography from '@material-ui/core/Typography'
@@ -13,11 +15,13 @@ const Container = styled.div`
 
 function App() {
     return (
-        <Container>
-            <Typography variant="h1">ATG TASK</Typography>
-            <GameTypeSearch />
-            <GamesList />
-        </Container>
+        <Provider store={store}>
+            <Container>
+                <Typography variant="h1">ATG TASK</Typography>
+                <GameTypeSearch />
+                <GamesList />
+            </Container>
+        </Provider>
     )
 }
 
