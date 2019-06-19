8ee57bb4e7d212738d58c4ab954e4801da7ac00b
cubbk <danpopa1998@gmail.com>
2019-06-19 18:56:15 +0300

    separate GameCardStateful from GamesList

---
+++ src/containers/GameCardStateful.js
@@ -0,0 +1,66 @@
+import React, { useReducer } from 'react'
+import { GameCard, RaceCard } from 'components/GameCard'
+import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button'
+import { createReducer } from 'redux-starter-kit'
+import { apiUrl } from 'constants.js'
+import axios from 'axios'
+
+export function GameCardStateful(props) {
+  const gameDefaultState = { data: {}, loading: false, error: null }
+
+  const gameReducer = createReducer(gameDefaultState, {
+      loading: (state, action) => {
+          state.loading = true
+          state.data = {}
+          state.error = null
+      },
+      fulfilled: (state, action) => {
+          state.data = action.payload
+          state.loading = false
+      },
+      error: (state, action) => {
+          state.loading = false
+          state.error = action.payload
+      },
+  })
+
+  const [game, gameDispatch] = useReducer(gameReducer, gameDefaultState)
+
+  async function handleMoreClick() {
+      console.log('triggered')
+      gameDispatch({ type: 'loading' })
+      try {
+          const request = await axios.get(`${apiUrl}games/${props.game.id}`)
+          gameDispatch({ type: 'fulfilled', payload: request.data })
+          console.log(request.data)
+      } catch (err) {
+          gameDispatch({ type: 'error', payload: err.message })
+      }
+  }
+  if (game.error) {
+      return (
+          <GameCard game={props.game} key={props.game.id}>
+              <CardActions>
+                  <Button onClick={handleMoreClick}>More</Button>
+              </CardActions>
+              {game.error}
+          </GameCard>
+      )
+  }
+  return (
+      <GameCard game={props.game} key={props.game.id}>
+          {game.loading === false && Object.entries(game.data).length === 0 && (
+              <CardActions>
+                  <Button onClick={handleMoreClick}>More</Button>
+              </CardActions>
+          )}
+          {game.loading && 'Loading'}
+          {Object.entries(game.data).length !== 0 && mapRaces(game.data)}
+      </GameCard>
+  )
+}
+
+function mapRaces(data) {
+  return data.races.map(race => <RaceCard race={race} key={race.id} />)
+}
--- src/containers/GamesList.js
+++ src/containers/GamesList.js
@@ -1,12 +1,7 @@
-import React, { useReducer } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
-import { GameCard, RaceCard } from 'components/GameCard'
 import Typography from '@material-ui/core/Typography'
-import CardActions from '@material-ui/core/CardActions'
-import Button from '@material-ui/core/Button'
-import { createReducer } from 'redux-starter-kit'
-import { apiUrl } from 'constants.js'
-import axios from 'axios'
+import { GameCardStateful } from './GameCardStateful'
 
 export function GamesList() {
     const upcomingGames = useSelector(state => state.upcomingGames)
@@ -39,7 +34,7 @@ export function GamesList() {
                     No upcoming games, showing results games
                 </Typography>
                 {resultsGames.data.map(race => (
-                  <GameCardStateful race={race} key={race.id} />
+                    <GameCardStateful race={race} key={race.id} />
                 ))}
             </React.Fragment>
         )
@@ -55,57 +50,4 @@ export function GamesList() {
             ))}
         </React.Fragment>
     )
-}
-
-function GameCardStateful(props) {
-    const gameDefaultState = { data: {}, loading: false, error: null }
-
-    const gameReducer = createReducer(gameDefaultState, {
-        loading: (state, action) => {
-            state.loading = true
-            state.data = {}
-            state.error = null
-        },
-        fulfilled: (state, action) => {
-            state.data = action.payload
-            state.loading = false
-        },
-        error: (state, action) => {
-            state.loading = false
-            state.error = action.payload
-        },
-    })
-
-    const [game, gameDispatch] = useReducer(gameReducer, gameDefaultState)
-
-    async function handleMoreClick() {
-      console.log('triggered')
-        gameDispatch({ type: 'loading' })
-        try {
-            const request = await axios.get(`${apiUrl}games/${props.race.id}`)
-            gameDispatch({ type: 'fulfilled', payload: request.data })
-            console.log(request.data)
-        } catch (err) {
-            gameDispatch({ type: 'error', payload: err.message })
-        }
-    }
-
-    return (
-        <GameCard
-            game={props.game}
-            key={props.game.id}
-        >
-            {game.loading === false && Object.entries(game.data).length === 0 && (
-                <CardActions>
-                    <Button onClick={handleMoreClick}>More</Button>
-                </CardActions>
-            )}
-            {game.loading && 'Loading'}
-            {Object.entries(game.data).length !== 0 && 123}
-        </GameCard>
-    )
-}
-
-function mapRaces(data) {
-  return data.races.map(race => <RaceCard race={race} key={race.id}/>)
 }
